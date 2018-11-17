5792b6a7b82e4bed9d6fb46333f7177b97a8a186
olsoa4 <olsoa4@hotmail.com>
2018-11-16 23:10:34 -0600

    ada

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,41 @@
+var logger = require("morgan");
+var mongoose = require("mongoose");
+var db = require("../models");
+require("dotenv").config();
+var user = process.env.USER;
+var password = process.env.PASSWORD;
+var mlab =
+  "mongodb://" +
+  user +
+  ":" +
+  password +
+  "@ds137687.mlab.com:37687/mongoscraperkg";
+var databaseUrl = mlab;
+mongoose.connect(databaseUrl);
+module.exports = function(app) {
+  app.get("/", function(req, res) {
+    db.Article.find({ saved: false })
+      .then(function(dbArticle) {
+        var hbsObject = {
+          articles: dbArticle
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+      })
+      .catch(function(err) {
+        console.log(err);
+      });
+  });
+  app.get("/saved", function(req, res) {
+    db.Article.find({ saved: true })
+      .then(function(dbArticle) {
+        var hbsObject = {
+          articles: dbArticle
+        };
+        res.render("saved", hbsObject);
+      })
+      .catch(function(err) {
+        console.log(err);
+      });
+  });
+};
